44556c9a1bde09b95813c11149e3e08ec470bd43
AbsoluteVirtueXI <absolutevirtuexi@gmail.com>
2020-11-13 15:43:23 +0100

    ...

--- contracts/SilverToken.sol
+++ contracts/SilverToken.sol
@@ -3,7 +3,10 @@ pragma solidity ^0.6.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract SilverToken is ERC20 {
-    constructor() public ERC20("SilverToken", "SLV") {
-        // TODO
+    address private _owner;
+
+    constructor(uint256 initialSupply) public ERC20("SilverToken", "SLV") {
+        _owner = msg.sender;
+        _mint(msg.sender, initialSupply);
     }
 }
