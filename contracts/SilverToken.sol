cf66ed5f1439450bf104b66815c09f99a680fc59
AbsoluteVirtueXI <absolutevirtuexi@gmail.com>
2020-11-13 17:01:08 +0100

    last version

--- contracts/SilverToken.sol
+++ contracts/SilverToken.sol
@@ -2,8 +2,9 @@
 pragma solidity ^0.6.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
 
-contract SilverToken is ERC20, Ownable {
+contract SilverToken is ERC20Burnable, Ownable {
     constructor(address owner_, uint256 initialSupply) public ERC20("SilverToken", "SLV") {
         transferOwnership(owner_);
         _mint(owner(), initialSupply);
