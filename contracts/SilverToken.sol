72b4b54f4bc64d3d50a1860c7304b16a728145e1
AbsoluteVirtueXI <absolutevirtuexi@gmail.com>
2020-11-16 12:28:04 +0100

    ...

--- contracts/SilverToken.sol
+++ contracts/SilverToken.sol
@@ -1,12 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Capped.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20Burnable.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Pausable.sol";
 
-contract SilverToken is ERC20Burnable, Ownable {
-    constructor(address owner_, uint256 initialSupply) public ERC20("SilverToken", "SLV") {
+contract SilverToken is ERC20Capped, ERC20Burnable, Ownable {
+    constructor(
+        address owner_,
+        uint256 initialSupply,
+        uint256 cap_
+    ) public ERC20("Silver", "SLV") ERC20Capped(cap_) {
         transferOwnership(owner_);
         _mint(owner(), initialSupply);
+    }
+
+    function mint(address account, uint256 amount) public onlyOwner {
+        _mint(account, amount);
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 amount
+    ) internal override(ERC20Capped, ERC20) {
+        super._beforeTokenTransfer(from, to, amount);
     }
 }
--- test/SilverToken_test.js
+++ test/SilverToken_test.js
@@ -10,24 +10,47 @@ describe('SilverToken', async function () {
   const SYMBOL = 'SLV';
   const INITIAL_SUPPLY = new BN('1000000' + '0'.repeat(18));
   const [owner, dev] = accounts;
+  context('context 1', async function () {
+    beforeEach(async function () {
+      this.silver = await SilverToken.new(owner, INITIAL_SUPPLY, { from: dev });
+    });
 
-  beforeEach(async function () {
-    this.silver = await SilverToken.new(owner, INITIAL_SUPPLY, { from: dev });
+    it('has name', async function () {
+      expect(await this.silver.name()).to.equal(NAME);
+    });
+
+    it('has symbol', async function () {
+      expect(await this.silver.symbol()).to.equal(SYMBOL);
+    });
+
+    it('transfers ownership from msg.sender to owner', async function () {
+      expect(await this.silver.owner()).to.equal(owner);
+    });
+
+    it('mints initial supply to owner', async function () {
+      expect(await this.silver.balanceOf(owner)).to.be.a.bignumber.equal(INITIAL_SUPPLY);
+    });
   });
+  context('context 2', async function () {
+    beforeEach(async function () {
+      console.log('for each tests in context 2');
+      this.silver = await SilverToken.new(owner, INITIAL_SUPPLY, { from: dev });
+    });
 
-  it('has name', async function () {
-    expect(await this.silver.name()).to.equal(NAME);
-  });
+    it('has name', async function () {
+      expect(await this.silver.name()).to.equal(NAME);
+    });
 
-  it('has symbol', async function () {
-    expect(await this.silver.symbol()).to.equal(SYMBOL);
-  });
+    it('has symbol', async function () {
+      expect(await this.silver.symbol()).to.equal(SYMBOL);
+    });
 
-  it('transfers ownership from msg.sender to owner', async function () {
-    expect(await this.silver.owner()).to.equal(owner);
-  });
+    it('transfers ownership from msg.sender to owner', async function () {
+      expect(await this.silver.owner()).to.equal(owner);
+    });
 
-  it('mints initial supply to owner', async function () {
-    expect(await this.silver.balanceOf(owner)).to.be.a.bignumber.equal(INITIAL_SUPPLY);
+    it('mints initial supply to owner', async function () {
+      expect(await this.silver.balanceOf(owner)).to.be.a.bignumber.equal(INITIAL_SUPPLY);
+    });
   });
 });
